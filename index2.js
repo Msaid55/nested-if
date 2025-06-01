3ed2f80d26c3ec6e0b380510c0b45b7db5b5d5d2
mohaj-dev <mohajali91@gmail.com>
2025-06-01 20:30:11 +0300

    nisted-if

---
+++ index2.js
@@ -0,0 +1,21 @@
+let answer = +prompt('Enter your degree');
+
+if (answer > -1) {
+  if (answer <= 100) {
+    if (answer < 50) {
+      console.log("فاشل");
+    } else if (answer < 65) {
+      console.log("مقبول");
+    } else if (answer < 75) {
+      console.log("جيد");
+    } else if (answer < 85) {
+      console.log("جيد جدا");
+    } else {
+      console.log("امتياز");
+    }
+  } else {
+    console.log("درجة غير صالحة");
+  }
+} else {
+  console.log("درجة غير صالحة");
+}
